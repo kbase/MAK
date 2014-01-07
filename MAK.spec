c14756ea938c5c8e8dea136abe53dc3712fbf8f7
mj <marcin@MacBookPro-20C9D0455F3D-3.local>
2014-01-07 11:49:21 -0800

    next version

---
+++ MAK.spec
@@ -0,0 +1,164 @@
+/* 
+	Module MAKBiclusterCollection version 1.0
+	This module provides access to MAK biclustering results.
+
+	@author marcin
+*/
+
+module MAK {
+		
+		
+	/* MAK dataset source 
+	
+		string taxon - NCBI taxonomy id
+		string id - kbase id
+		string description - description
+		string dataPath - path to data
+	    int num_rows - number of rows
+	    int num_columns - number of columns
+	     	     
+		@searchable ws_subset taxon id description
+	*/
+	typedef structure {	   
+		string taxon;	 
+		string id;
+		string data_type;
+		string description;	
+		string dataPath;	
+		int num_rows;
+		int num_cols;		
+  	} MAKInputData;  
+	
+	
+	
+	/* MAK algorithm and discovery strategy parameters 
+	
+		float min_raw_bicluster_score - minimum raw bicluster score
+		float max_bicluster_overlap - maximum allowed bicluster overlap
+		float max_enrich_pvalue - maximum allowed enrichment p-value
+	    int rounds - number of rounds in discovery strategy
+	    list<string> rounds_move_sequences - 
+	    int refine - refinement y/n 
+	    string linkage - complete, single, mean etc.
+	    string null_data_path - path to null distribution files
+	    string Rcodepath - path to R code (Miner.R)
+	    string Rdatapath - path to Rdata object
+	    list<MAKInputData> inputs - objects for MAK input data
+		
+		@optional 
+		
+    	@searchable ws_subset
+	*/
+  	typedef structure {  	
+		float min_raw_bicluster_score;
+		float max_bicluster_overlap;
+		float max_enrich_pvalue;
+		int rounds;		
+		list<string> rounds_move_sequences;
+		int refine;
+		string linkage;
+		string null_data_path;
+		string Rcodepath;
+		string Rdatapath;						
+		list<MAKInputData> inputs;		
+  	} MAKParameters;  
+  
+	
+
+	/* Bicluster 
+	
+		string bicluster_type - type of bicluster (determined by source data, e.g. expression, fitness, metagenomic, metabolite, integrated)
+		string bicluster_id - id
+		int num_genes - number of genes
+		int num_conditions - number of conditions
+		list<string> condition_ids - condition ids
+		list<string> condition_labels - condition labels
+		list<string> gene_ids - gene ids
+		list<string> gene_labels - gene labels
+		float exp_mean - expression mean
+		float exp_mean_crit - expression mean criterion value
+		float exp_crit - expression criterion value
+		float ppi_crit - PPI criterion value
+		float TF_crit - TF criterion value
+		float ortho_crit - orthology criterion value
+		float full_crit - full criterion value
+		float miss_frxn - fraction of missing data
+		mapping<string, string> enriched_terms - enriched terms
+	
+		@searchable ws_subset bicluster_id gene_ids gene_labels condition_ids condition_labels enriched_terms
+	*/
+   typedef structure {			 
+	 string bicluster_id;	
+	 int num_genes;
+	 int num_conditions;
+	 list<string> condition_ids;	
+	 list<string> condition_labels;	
+	 list<string> gene_ids;	 
+	 list<string> gene_labels;	
+	 float exp_mean;	
+	 float exp_mean_crit;	
+	 float exp_crit;
+	 float ppi_crit;
+	 float TF_crit;
+	 float ortho_crit;
+	 float full_crit;
+	 float miss_frxn;
+	 mapping<string, string> enriched_terms;
+	} MAKBicluster;
+
+
+
+	/*Bicluster annotations -- for future upgrade, currently rely on map<string, string> in MAKBicluster 
+	
+		WARNING -- placeholder for future implementation
+	
+		list<list<string>> GO - list of GO terms enriched in this set of genes
+		list<list<string>> KEGG - list of KEGG pathways enriched in this set of genes
+		list<list<string>> TIGRFAM_role - list of TIGRFAM roles enriched in this set of genes
+		list<list<string>> TF_binding_sites - list of TFs with binding sites in this set of genes
+	
+		@searchable ws_subset GO KEGG TIGRFAM_role TF_binding_sites
+	*/
+	typedef structure {
+	   list<list<string>> GO;
+	   list<list<string>> KEGG;
+	   list<list<string>> TIGRFAM_role;
+	   list<list<string>> TF_binding_sites;		
+  	} MAKBiclusterAnnotation;  
+	
+	
+	
+	/* Bicluster set 
+	
+		string id - id
+		string time_stamp - time stamp for the results
+		string version - MAK version
+		int number - number of biclusters in set
+		int min_genes - min genes for bicluster in set
+		int max_genes - max genes for bicluster in set
+		int min_conditions - max genes for bicluster in set
+		int max_conditions - max conditions for bicluster in set
+		string taxon - NCBI taxonomy id
+		string bicluster_type - type of bicluster (determined by source data, e.g. expression, fitness, metagenomic, metabolite, integrated)		
+		list<MAKBicluster> biclusters - biclusters
+		list<MAKBiclusterAnnotation> annotations - bicluster annotations
+		MAKParameters mak_param - parameters
+		
+	@searchable ws_subset id taxon
+	*/
+	typedef structure {	
+	    string id;
+		string time_stamp;
+		string version;
+		int number;
+		int min_genes;
+		int max_genes;
+		int min_conditions;
+		int max_conditions;
+	 	string taxon;		
+	 	string bicluster_type;
+		list<MAKBicluster> biclusters;
+		list<MAKBiclusterAnnotation> annotations;		
+		MAKParameters mak_param;		
+  	} MAKBiclusterSet; 
+};
---
+++ src/us/kbase/mak/BiclusterstoJSON.java
@@ -0,0 +1,313 @@
+package us.kbase.mak;
+
+import DataMining.*;
+import mathy.SimpleMatrix;
+import util.MoreArray;
+import util.TabFile;
+import util.TextFile;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Marcin Joachimiak
+ * User: marcin
+ * Date: 12/17/13
+ * Time: 11:37 PM
+ */
+public class BiclusterstoJSON {
+
+    ValueBlockList vbl;
+    Parameters prm;
+    DiscoveryParameters dprm;
+    String[][] summary_data;
+    SimpleMatrix sm;
+    String[] gene_labels;
+    String[] exp_labels;
+
+    MAKBiclusterSet mbs;
+
+    java.util.Date date;
+
+    String default_bicluster_type = "expression";
+    String default_biclusterset_id = "" + 0;
+    String default_biclusterset_desc = "gene expression biclusters";
+
+
+    /**
+     * @param args
+     */
+    public BiclusterstoJSON(String[] args) {
+
+        try {
+            loadInputs(args);
+
+            int max_conditions = -1;
+            int max_genes = -1;
+            int num = vbl.size();
+
+            mbs = new MAKBiclusterSet();
+            mbs.setId("" + 0);
+            mbs.setMaxConditions(max_conditions);
+            mbs.setMaxGenes(max_genes);
+            mbs.setMinConditions(max_conditions);
+            mbs.setMinGenes(max_genes);
+            mbs.setNumber(num);
+            //mbs.setAnnotations();
+            mbs.setBiclusterType(default_bicluster_type);
+            mbs.setTaxon(dprm.taxon);
+            mbs.setTimeStamp((new Timestamp(date.getTime())).toString());
+            mbs.setVersion(MINER_STATIC.version);
+
+           /* List<MAKBiclusterAnnotation> mbalist = new ArrayList();
+            List<List<String>> listannotmaster = new ArrayList();
+            List<String> listannot = new ArrayList();
+            listannot.add("");
+            for (int i = 0; i < vbl.size(); i++) {
+                listannotmaster.add(listannot);
+            }
+
+            doAnnotations(mbalist, listannotmaster);*/
+
+            doBiclusters();
+
+            List<MAKInputData> listinput = doInputs();
+
+            doParams(listinput);
+
+            TextFile.write(UObject.transformObjectToString(mbs), args[0] + "_MAKBiclusterSet.json");
+            //TextFile.write(mbs.toString(), args[0] + "_MAKBiclusterSet.json");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * @param mbalist
+     * @param listannotmaster
+     */
+    private void doAnnotations(List<MAKBiclusterAnnotation> mbalist, List<List<String>> listannotmaster) {
+        MAKBiclusterAnnotation mba = new MAKBiclusterAnnotation();
+        mba.setGO(listannotmaster);
+        mba.setTIGRFAMRole(listannotmaster);
+        mba.setKEGG(listannotmaster);
+        mba.setTFBindingSites(listannotmaster);
+        mbalist.add(mba);
+        mbs.setAnnotations(mbalist);
+    }
+
+    /**
+     *
+     */
+    private void doBiclusters() {
+        List<MAKBicluster> biclust = new ArrayList<MAKBicluster>();//vbl.size()
+        for (int i = 0; i < vbl.size(); i++) {
+            ValueBlock vb = (ValueBlock) vbl.get(i);
+            if (vb == null)
+                System.out.println("vb is null");
+            MAKBicluster mb = new MAKBicluster();
+            mb.setBiclusterId("" + i);
+            mb.setConditionIds(MoreArray.convtoArrayList(MoreArray.toStringArray(vb.exps)));
+            if (exp_labels != null) {
+                List<String> clabels = new ArrayList();
+                for (int g = 0; g < vb.exps.length; g++) {
+                    String s = exp_labels[vb.exps[g] - 1];
+                    clabels.add(s);
+                }
+                mb.setConditionLabels(clabels);
+            } else {
+                mb.setConditionLabels(MoreArray.convtoArrayList(MoreArray.toStringArray(vb.exps)));
+            }
+
+            mb.setGeneIds(MoreArray.convtoArrayList(MoreArray.toStringArray(vb.genes)));
+            if (gene_labels != null) {
+                List<String> glabels = new ArrayList();
+                for (int g = 0; g < vb.genes.length; g++) {
+                    String s = gene_labels[vb.genes[g] - 1];
+                    glabels.add(s);
+                }
+                mb.setGeneLabels(glabels);
+            } else {
+                mb.setGeneLabels(MoreArray.convtoArrayList(MoreArray.toStringArray(vb.genes)));
+            }
+
+           /* vb.setDataAndMean(sm.data);
+            List<List<String>> listdata = new ArrayList();// = MoreArray.initArrayList((vb.exp_data.length));
+            for (int g = 0; g < vb.exp_data.length; g++) {
+                ArrayList cur = MoreArray.convtoArrayList(MoreArray.toStringArray(vb.exp_data[g]));//new ArrayList();
+                *//*for (int c = 0; c < cur.size(); c++) {
+                    Double d = (Double) cur.get(c);
+                    if (d.isNaN())
+                        d = null;
+                    cur.set(c, d.toString());
+                }*//*
+                //if (g == 0)
+                //    MoreArray.printArray(vb.exp_data[g]);
+                listdata.add(cur);
+            }
+
+            mb.setData(listdata);*/
+
+            mb.setNumConditions(vb.exps.length);
+            mb.setNumGenes(vb.genes.length);
+            mb.setMissFrxn(vb.frxnNaN());
+
+            Map terms = new HashMap();
+            //GO terms
+            String allterms = summary_data[i][12];
+            String[] termlist = new String[0];
+            try {
+                termlist = allterms.substring(4).split("_");
+                for (int t = 0; t < termlist.length; t++) {
+                    terms.put("GO", termlist[t]);
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
+                System.out.println("allterms GO " + allterms);
+            }
+
+            //TIGRFam roles
+            allterms = summary_data[i][10];
+            try {
+                termlist = allterms.substring(10).split("_");
+
+                for (int t = 0; t < termlist.length; t++) {
+                    terms.put("TIGRFam", termlist[t]);
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
+                System.out.println("allterms TIGRFam " + allterms);
+            }
+
+            mb.setEnrichedTerms(terms);
+            mb.setExpCrit((vb.all_criteria[ValueBlock_STATIC.expr_mse_IND] + vb.all_criteria[ValueBlock_STATIC.expr_reg_IND] +
+                    vb.all_criteria[ValueBlock_STATIC.expr_kend_IND]) / 3.0);
+            mb.setExpMean(vb.exp_mean);
+            mb.setExpMeanCrit(vb.all_criteria[ValueBlock_STATIC.expr_mean_IND]);
+            mb.setFullCrit(vb.full_criterion);
+            mb.setGeneIds(MoreArray.convtoArrayList(MoreArray.toStringArray(vb.genes)));
+            //mb.setGeneLabels();
+            mb.setOrthoCrit(vb.all_criteria[ValueBlock_STATIC.feat_IND]);
+            mb.setPpiCrit(vb.all_criteria[ValueBlock_STATIC.INTERACT_IND]);
+            mb.setTFCrit(vb.all_criteria[ValueBlock_STATIC.TF_IND]);
+
+            biclust.add(mb);
+        }
+
+        mbs.setBiclusters(biclust);
+    }
+
+    /**
+     * @param listinput
+     */
+    private void doParams(List<MAKInputData> listinput) {
+        MAKParameters makp = new MAKParameters();
+        makp.setInputs(listinput);
+        makp.setLinkage(dprm.linkage);
+        makp.setMaxBiclusterOverlap(dprm.max_bicluster_overlap);
+        makp.setMaxEnrichPvalue(dprm.max_enrich_pvalue);
+        makp.setMinRawBiclusterScore(dprm.min_raw_bicluster_score);
+        makp.setNullDataPath(dprm.null_data_path);
+        makp.setRcodepath(prm.R_METHODS_PATH);
+        makp.setRdatapath(prm.R_DATA_PATH);
+        makp.setRefine((dprm.refine ? 1 : 0));
+        makp.setRounds(dprm.rounds);
+        makp.setRoundsMoveSequences(MoreArray.convtoArrayList(dprm.move_sequences));
+
+        mbs.setMakParam(makp);
+    }
+
+    /**
+     * @return
+     */
+    private List<MAKInputData> doInputs() {
+        /* TODO support multiple inputs */
+        MAKInputData maki = new MAKInputData();
+        maki.setDataPath(prm.EXPR_DATA_PATH);
+        maki.setDataType(default_bicluster_type);
+        maki.setDescription(default_biclusterset_desc);
+        maki.setId(default_biclusterset_id);
+        maki.setNumCols(sm.data[0].length);
+        maki.setNumRows(sm.data.length);
+        maki.setTaxon(dprm.taxon);
+        List<MAKInputData> listinput = new ArrayList();
+        listinput.add(maki);
+        return listinput;
+    }
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    private void loadInputs(String[] args) throws Exception {
+        vbl = ValueBlockListMethods.readAny(args[0]);
+
+        System.out.println("Loaded " + vbl.size());
+
+        summary_data = TabFile.readtoArray(args[1]);
+
+        sm = new SimpleMatrix(args[2]);
+
+        prm = new Parameters();
+        prm.read(args[3]);
+
+        dprm = new DiscoveryParameters();
+        dprm.read(args[4]);
+
+        if (args.length >= 6) {
+            try {
+                String[][] sarray = TabFile.readtoArray(args[3]);
+                System.out.println("setLabels g " + sarray.length + "\t" + sarray[0].length);
+                int col = 2;
+                String[] n = MoreArray.extractColumnStr(sarray, col);
+                gene_labels = MoreArray.replaceAll(n, "\"", "");
+                System.out.println("setLabels gene " + gene_labels.length + "\t" + gene_labels[0]);
+            } catch (Exception e) {
+                System.out.println("expecting 2 cols");
+                e.printStackTrace();
+            }
+            if (args.length == 7) {
+                try {
+                    String[][] sarray = TabFile.readtoArray(args[3]);
+                    System.out.println("setLabels g " + sarray.length + "\t" + sarray[0].length);
+                    int col = 2;
+                    String[] n = MoreArray.extractColumnStr(sarray, col);
+                    exp_labels = MoreArray.replaceAll(n, "\"", "");
+                    System.out.println("setLabels gene " + exp_labels.length + "\t" + exp_labels[0]);
+                } catch (Exception e) {
+                    System.out.println("expecting 2 cols");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        date = new java.util.Date();
+    }
+
+
+    /**
+     * @paramfile args
+     */
+
+    public final static void main(String[] args) {
+        if (args.length == 5 || args.length == 7) {
+            BiclusterstoJSON rm = new BiclusterstoJSON(args);
+        } else {
+            System.out.println("syntax: java us.kbase.mak.BiclusterstoJSON\n" +
+                    "< MAK merged .vbl file >\n" +
+                    "< MAK summary file >\n" +
+                    "< primary input data>\n" +
+                    "< MAK trajectory parameter file >\n" +
+                    "< MAK discovery strategy file >\n" +
+                    "< OPTIONAL primary input data gene labels >\n" +
+                    "< OPTIONAL (only if also gene labels) primary input data condition labels >"
+            );
+        }
+    }
+}
